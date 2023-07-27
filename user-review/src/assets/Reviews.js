b3028683c0b170e61f0757aaa8ff5026de9e4f68
Chris <user@example.com>
2023-07-27 11:00:52 -0600

    updating data set lol

---
+++ user-review/src/assets/Reviews.js
@@ -0,0 +1,22 @@
+const reviews = [
+{
+    name: "Ana",
+    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu efficitur ipsum. Pellentesque sed diam id orci luctus laoreet. Sed auctor ipsum et augue pretium molestie. Aliquam ante magna, euismod in lectus sit amet, vestibulum lacinia felis. Aenean euismod eros nisl, vel tempus sapien convallis ut. Quisque maximus egestas volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc id enim sed turpis rutrum viverra. Donec ut mollis nunc, cursus varius mi. In hendrerit semper mi ut malesuada. Integer et enim augue."
+}, 
+{
+    name: "Chris",
+    review: "Nulla ac ante ut dui ullamcorper tristique. Ut pulvinar vel lectus sed malesuada. Donec id pulvinar ligula. Vivamus sed risus commodo, aliquet mi eu, mollis quam. Proin eros mauris, imperdiet at rutrum at, lobortis at mi. Pellentesque eu magna non velit convallis porta nec vulputate leo. Aliquam lacinia ornare molestie. Quisque eu est sit amet elit blandit dapibus."
+}, 
+{
+    name: "Gaby",
+    review: "Etiam non pellentesque dui, quis placerat eros. Nullam nec auctor nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus cursus a nunc ac facilisis. Aliquam vitae nibh sapien. Morbi congue ex sed nibh suscipit, id blandit enim auctor. Integer leo nisi, luctus at tincidunt posuere, aliquam at lacus."
+}, 
+{
+    name: "Mr T",
+    review: "hasellus ipsum massa, scelerisque vitae tempus eu, ullamcorper quis sapien. Curabitur sed varius mi, in placerat purus. Maecenas purus nunc, finibus placerat ornare sit amet, mollis et sem. Aliquam dignissim nibh vel nisi condimentum porta. Nam faucibus volutpat porttitor. Donec vel mi consectetur, condimentum diam et, sagittis enim. Praesent sed tortor sed metus consequat dapibus ac ut sapien. Nulla blandit libero vitae quam imperdiet, nec fringilla est malesuada. Sed malesuada sed lorem et dictum. Nulla placerat rhoncus lorem, eu bibendum tortor pulvinar sit amet. Curabitur interdum sit amet ipsum ac condimentum."
+}, 
+{
+    name: "Amanda",
+    review: "Sed tellus dui, consequat non gravida vel, porttitor eget nisl. Phasellus augue lorem, laoreet suscipit dui in, lobortis tempus tortor. Etiam eget tincidunt nisi. Morbi sed varius tortor, quis congue diam. Sed accumsan et augue ut consequat. Ut sed neque dignissim, mollis neque et, tristique elit. Aliquam sed ligula purus. Donec vitae egestas nunc. Donec at lacinia metus. Integer non lacus aliquet mi ullamcorper placerat."
+}
+]
