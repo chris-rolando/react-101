2d3b836743594e7b0514ca17bb571b8792264186
Chris <user@example.com>
2023-07-27 10:26:14 -0600

    working on user review app

--- user-review/src/App.js
+++ user-review/src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
 <h1>Reseñas</h1>
+      <Review 
+        user_name="Ana" 
+        user_review="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis dui enim, sed rhoncus ligula dapibus et. Praesent cursus ornare molestie. Integer vestibulum leo a nulla dignissim, eget iaculis purus placerat. Phasellus sodales purus nec diam venenatis mattis. Fusce varius, erat eget molestie gravida, urna nunc placerat sem, vel lacinia ante nisi eu sapien. Vivamus vitae elit sit amet ipsum vestibulum aliquam vel et ipsum. Nullam porttitor sem ac lectus ornare tempor. Pellentesque suscipit, massa sit amet sodales pharetra, justo dolor condimentum ligula, id mattis nunc diam id urna. Sed at augue quis tortor varius condimentum. Praesent at augue eu lorem condimentum rutrum sit amet vitae metus. Quisque rutrum rhoncus nulla at ultrices. Vivamus dignissim dictum nisi a molestie."
+      />
+      <Review />
       <Review />
     </div>
   );
--- user-review/src/components/ReviewBox.jsx
+++ user-review/src/components/ReviewBox.jsx
@@ -1,19 +1,27 @@
-import photo from '../assets/account.png';
-import '../styles/ReviewBox.css';
+import PropTypes from "prop-types";
+import photo from "../assets/account.png";
+import "../styles/ReviewBox.css";
 
-export const Review = () => {
-  
+export const Review = ({ user_avatar, user_name, user_review }) => {
   return (
-  <article className="review-box">
-    <img 
-        src={photo}
-        className="App-logo"
-        alt="logo"
-    />
-    <div className='App-text'> 
-    <h3 className="review-title">Thanos</h3>
-    <p className="review-test">Soy un functional component en react</p>
-    </div>
-  </article>
+    <article className="review-box">
+      <img src={user_avatar} className="App-logo" alt={`foto de ${user_name}` }/>
+      <div className="App-text">
+        <h3 className="review-title">{user_name}</h3>
+        <p className="review-test">"{user_review}"</p>
+      </div>
+    </article>
   );
+};
+
+Review.propTypes = {
+  user_avatar: PropTypes.string,
+  user_name: PropTypes.string.isRequired,
+  user_review: PropTypes.string.isRequired
+};
+
+Review.defaultProps = { 
+  user_avatar: photo,
+  user_name: "Chris",
+  user_review: "I'm using React"
 };
